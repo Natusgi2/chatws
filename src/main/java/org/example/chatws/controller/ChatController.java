2ea4199e8778625942fbc8490534d5591ecc4aef
Miguel Agustin Vergara <costolero@gmail.com>
2026-09-17 19:17:15 -0300

    chat funcional en local mediante websockets

---
+++ src/main/java/org/example/chatws/controller/ChatController.java
@@ -0,0 +1,25 @@
+package org.example.chatws.controller;
+
+import org.example.chatws.entity.ChatMessage;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class ChatController {
+    @MessageMapping ("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage enviarMensaje(ChatMessage mensaje) {
+        System.out.println("Mensaje recibido: " + mensaje);
+        return mensaje; 
+    } 
+
+    @MessageMapping ("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage agregarUsuario(ChatMessage mensaje) {
+        //System.out.println("Usuario agregado: " + mensaje);
+        return mensaje; 
+    }
+    
+}
---
+++ src/main/java/org/example/chatws/entity/ChatMessage.java
@@ -0,0 +1,7 @@
+package org.example.chatws.entity;
+
+public record ChatMessage(String tipo, String usuario, String contenido) {
+
+    
+    
+}
